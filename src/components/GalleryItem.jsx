dc5fdfc6ee4ee4c87812f9d1c2181209628ba63f
JVC21 <jvichecastillo@gmail.com>
2026-01-06 18:13:30 -0500

    feat(gallery page): gallery distribution new idea

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,39 +1,5 @@
-function GalleryItem({ id, colSpan, rowSpan, colStart, rowStart }) {
-    const colSpanMap = {
-        1: 'col-span-1',
-        2: 'col-span-2',
-        4: 'col-span-4'
-    }
-
-    const rowSpanMap = {
-        1: 'row-span-1',
-        2: 'row-span-2',
-        3: 'row-span-3',
-        4: 'row-span-4'
-    }
-
-    const colStartMap = {
-        1: 'col-start-1',
-        3: 'col-start-3',
-        5: 'col-start-5',
-        6: 'col-start-6',
-        8: 'col-start-8',
-        9: 'col-start-9',
-        11: 'col-start-11'
-    }
-
-    const rowStartMap = {
-        1: 'row-start-1',
-        2: 'row-start-2',
-        3: 'row-start-3',
-        4: 'row-start-4',
-        5: 'row-start-5',
-        6: 'row-start-6',
-        7: 'row-start-7',
-        8: 'row-start-8'
-    }
-
-    return <div className={`${colSpanMap[colSpan]} ${rowSpanMap[rowSpan]} ${colStartMap[colStart]} ${rowStartMap[rowStart]} bg-neutral-400 rounded-lg flex items-center justify-center transition-all duration-300 shadow-lg cursor-pointer hover:scale-105`}></div>
+function GalleryItem({ id, className, children }) {
+    return <div id={id} className={`${className} bg-neutral-400 rounded-lg flex items-center justify-center shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}>{children}</div>
 }
 
 export default GalleryItem;
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -2,33 +2,33 @@ import GalleryItem from "../components/GalleryItem";
 
 function GalleryPage() {
     return (
-        <div className="grid grid-cols-12 grid-rows-8 gap-4 p-4 bg-neutral-200 min-h-screen">
-            <GalleryItem id={1} colSpan={4} rowSpan={4} colStart={5} rowStart={3} />
-            <GalleryItem id={2} colSpan={2} rowSpan={3} colStart={3} rowStart={1} />
-            <GalleryItem id={3} colSpan={2} rowSpan={3} colStart={9} rowStart={1} />
-            <GalleryItem id={4} colSpan={2} rowSpan={3} colStart={3} rowStart={6} />
-            <GalleryItem id={5} colSpan={2} rowSpan={3} colStart={9} rowStart={6} />
-            <GalleryItem id={6} colSpan={2} rowSpan={2} colStart={6} rowStart={1} />
-            <GalleryItem id={7} colSpan={2} rowSpan={2} colStart={6} rowStart={7} />
-            <GalleryItem id={8} colSpan={2} rowSpan={2} colStart={3} rowStart={4} />
-            <GalleryItem id={9} colSpan={2} rowSpan={2} colStart={9} rowStart={4} />
-            <GalleryItem id={10} colSpan={2} rowSpan={2} colStart={1} rowStart={3} />
-            <GalleryItem id={11} colSpan={2} rowSpan={2} colStart={11} rowStart={3} />
-            <GalleryItem id={12} colSpan={2} rowSpan={2} colStart={1} rowStart={5} />
-            <GalleryItem id={13} colSpan={2} rowSpan={2} colStart={11} rowStart={5} />
-            <GalleryItem id={14} colSpan={2} rowSpan={2} colStart={1} rowStart={1} />
-            <GalleryItem id={15} colSpan={2} rowSpan={2} colStart={11} rowStart={1} />
-            <GalleryItem id={16} colSpan={2} rowSpan={2} colStart={1} rowStart={7} />
-            <GalleryItem id={17} colSpan={2} rowSpan={2} colStart={11} rowStart={7} />
-            <GalleryItem id={18} colSpan={1} rowSpan={1} colStart={5} rowStart={2} />
-            <GalleryItem id={19} colSpan={1} rowSpan={1} colStart={8} rowStart={2} />
-            <GalleryItem id={20} colSpan={1} rowSpan={1} colStart={5} rowStart={7} />
-            <GalleryItem id={21} colSpan={1} rowSpan={1} colStart={8} rowStart={7} />
-            <GalleryItem id={18} colSpan={1} rowSpan={1} colStart={5} rowStart={1} />
-            <GalleryItem id={19} colSpan={1} rowSpan={1} colStart={8} rowStart={1} />
-            <GalleryItem id={20} colSpan={1} rowSpan={1} colStart={5} rowStart={8} />
-            <GalleryItem id={21} colSpan={1} rowSpan={1} colStart={8} rowStart={8} />
-		</div>
+        <div className="min-h-screen bg-neutral-300">
+            <div className="grid grid-cols-1 lg:grid-cols-5 xl:grid-cols-7 p-6 gap-6">
+                <div className="row-span-3 col-span-3 col-start-3 row-start-4 flex flex-col items-left justify-center z-20">
+                    <h2 className="text-center text-8xl outfit-regular mb-2 text-neutral-700 transition-all duration-300 hover:scale-105 hover:text-neutral-900 hover:text-shadow-lg">Pixel Flowers</h2>
+                    <p className="dm-mono-regular text-center text-neutral-800 transition-all duration-300 hover:scale-105">By Julio Viche</p>
+                </div>
+
+                <button className="col-start-2 aspect-7/3">Button 1</button>
+                <button className="col-start-4 aspect-7/3">Button 2</button>
+                <button className="col-start-6 aspect-7/3">Button 3</button   >
+
+                <GalleryItem className="col-start-1 row-start-1 row-span-2 aspect-square">1</GalleryItem>
+                <GalleryItem className="col-start-3 row-start-1 row-span-2 aspect-square">2</GalleryItem>
+                <GalleryItem className="col-start-5 row-start-1 row-span-2 aspect-square">3</GalleryItem>
+                <GalleryItem className="col-start-7 row-start-1 row-span-2 aspect-square">4</GalleryItem>
+                
+                {Array.from({ length: 16 }).map((_, i) => (
+                    <GalleryItem className="row-span-2 aspect-square" key={i}>{i + 5}</GalleryItem>
+                ))}
+
+                <GalleryItem className="col-start-4 row-start-8 row-span-2 aspect-square">21</GalleryItem>
+
+                {Array.from({ length: 19 }).map((_, i) => (
+                    <GalleryItem className="row-span-2 aspect-square" key={i}>{i + 22}</GalleryItem>
+                ))}
+            </div>
+        </div>
     );
 }
 
