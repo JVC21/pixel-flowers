d297e8897b50bd9e66621bb925329556aa7d9eb6
JVC21 <jvichecastillo@gmail.com>
2026-01-07 09:23:36 -0500

    feat(gallery item): change content

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,5 +1,5 @@
-function GalleryItem({ className, children }) {
-    return <div className={`${className} bg-neutral-400 rounded-lg flex items-center justify-center shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}>{children}</div>
+function GalleryItem({ flower, className }) {
+    return <div className={`${className} bg-neutral-400 rounded-lg flex items-center justify-center shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}>{flower.id}</div>
 }
 
 export default GalleryItem;
