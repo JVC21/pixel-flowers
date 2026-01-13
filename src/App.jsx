9eccc0b9f345f8e4c51355d29d5dce3e13b0d0bf
JVC21 <jvichecastillo@gmail.com>
2026-01-13 07:57:16 -0500

    feat: install and implement react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router"
 import CardPage from "./pages/CardPage.jsx"
 import GalleryPage from "./pages/GalleryPage.jsx"
 
 function App() {
 	return (
-		<GalleryPage />
+		<Routes>
+			<Route path="/" element={<GalleryPage />} />
+			<Route path="/flower" element={<CardPage />} />
+			{/* <Route path="/more" element={<MorePage />} /> */}
+		</Routes>
+
 	)
 }
 
