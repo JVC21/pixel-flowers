f1b20a3aaa49b3a867077b67ab323539902c0b47
JVC21 <jvichecastillo@gmail.com>
2026-01-23 16:40:34 -0500

    feat: validate image url in gallery item and card image

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -6,28 +6,48 @@ const images = import.meta.glob('../assets/*.jpg', { eager: true, query: '?url',
 function CardImage({ flower, isOpen, isPlaceholder = true }) {
     const imageUrl = images[`../assets/${isPlaceholder ? '480x480.jpg' : flower.image}`]
 
-    return (
-        <img
-            src={imageUrl}
-            alt={flower.common_name}
-            className={twMerge(clsx(
-                // Base styles
-                "bg-placeholder rounded-xl shadow-lg z-30",
-                // Minimal screens
-                "size-80",
-                isOpen && "rounded-none rounded-t-xl",
-                // Medium screens
-                "md:size-90",
-                // Large screens
-                "lg:size-105",
-                isOpen && "lg:rounded-none lg:rounded-l-xl",
-                // Extra large screens
-                "xl:size-120",
-                // Animation
-                "transition-all duration-300"
-            ))}
-        />
-    )
+    if (flower.image) 
+    return <img 
+        src={imageUrl} 
+        alt={flower.common_name}
+        className={twMerge(clsx(
+            // Base styles
+            "bg-placeholder rounded-xl shadow-lg z-30",
+            // Minimal screens
+            "size-80",
+            isOpen && "rounded-none rounded-t-xl",
+            // Medium screens
+            "md:size-90",
+            // Large screens
+            "lg:size-105",
+            isOpen && "lg:rounded-none lg:rounded-l-xl",
+            // Extra large screens
+            "xl:size-120",
+            // Animation
+            "transition-all duration-300"
+        ))}
+    />
+    
+    return <div
+        className={twMerge(clsx(
+            // Base styles
+            "flex justify-center items-center bg-placeholder rounded-xl shadow-lg z-30",
+            // Minimal screens
+            "size-80",
+            isOpen && "rounded-none rounded-t-xl",
+            // Medium screens
+            "md:size-90",
+            // Large screens
+            "lg:size-105",
+            isOpen && "lg:rounded-none lg:rounded-l-xl",
+            // Extra large screens
+            "xl:size-120",
+            // Animation
+            "transition-all duration-300"
+        ))}
+    >
+        <i className="fa-solid fa-seedling text-muted text-7xl lg:text-8xl xl:text-9xl transition-all duration-300"></i>
+    </div>
 }
 
 export default CardImage
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,7 +1,22 @@
 import { Link } from "react-router"
 
+const images = import.meta.glob('../assets/*.jpg', { eager: true, query: '?url', import: 'default' })
+
 function GalleryItem({ flower, className }) {
-    return <Link to={`/flowers/${flower.id}`} className={`${className} flex items-center justify-center bg-placeholder rounded-lg shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}></Link>
+    const imageUrl = images[`../assets/${flower.image}`]
+
+    if (flower.image) 
+    return <Link to={`/flowers/${flower.id}`} className={`${className} flex items-center justify-center bg-placeholder rounded-lg shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}>
+        <img
+            src={imageUrl}
+            alt={flower.common_name}
+            className="rounded-lg size-full object-cover"
+        />
+    </Link>
+    
+    return <Link to={`/flowers/${flower.id}`} className={`${className} flex items-center justify-center bg-placeholder rounded-lg shadow-lg cursor-pointer transition-all duration-300 hover:scale-105`}>
+        <i className="fa-solid fa-seedling text-muted text-5xl transition-all duration-300"></i>
+    </Link>
 }
 
 export default GalleryItem
