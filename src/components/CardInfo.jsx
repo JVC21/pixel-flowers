8e9272f9b6569f2c886aa8556126729f5d4748a1
JVC21 <jvichecastillo@gmail.com>
2026-01-23 16:45:48 -0500

    feat: validate flower information in card info

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -23,12 +23,12 @@ function CardInfo({ flower, isOpen }) {
             ))}
         >
             <div className="lg:min-w-54 xl:min-w-60">
-                <h2 className="cal-sans-regular text-body text-4xl tracking-wide mb-2">{flower.common_name}</h2>
-                <p className="outfit-regular-italic text-muted text-sm mb-6 xl:mb-8">{flower.scientific_name}</p>
-                <p className="outfit-regular text-body text-justify">{flower.description}</p>
+                <h2 className="cal-sans-regular text-body text-4xl tracking-wide mb-2">{flower.common_name ?? "Unknown"}</h2>
+                <p className="outfit-regular-italic text-muted text-sm mb-6 xl:mb-8">{flower.scientific_name ?? "No scientific name"}</p>
+                <p className="outfit-regular text-body text-justify">{flower.description ?? "No description available"}</p>
             </div>
             <div>
-                <p className="dm-mono-medium-italic text-strong text-xs text-right">{flower.date_added}</p>
+                <p className="dm-mono-medium-italic text-strong text-xs text-right">{flower.date_added ?? "Unknown date"}</p>
                 {/* <p className="outfit-bold text-strong mb-2">{flower.rating}</p> */}
             </div>
         </div>
