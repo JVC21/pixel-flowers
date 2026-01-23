d7b24b02e5c3e2a6cc30989317d205298b816103
JVC21 <jvichecastillo@gmail.com>
2026-01-22 22:33:18 -0500

    feat: disable galery buttons temporarily

--- src/components/GalleryButton.jsx
+++ src/components/GalleryButton.jsx
@@ -1,7 +1,8 @@
 
 function GalleryButton({ className, children }) {
     return (
-        <button className={`${className} outfit-regular text-lg text-body bg-accent rounded-lg cursor-pointer shadow-lg transition-all duration-300 hover:scale-105`}>{children}</button>
+        // <button className={`${className} outfit-regular text-lg text-body bg-accent rounded-lg cursor-pointer shadow-lg transition-all duration-300 hover:scale-105`}>{children}</button>
+        <button className={`${className} outfit-regular text-lg text-muted bg-accent rounded-lg cursor-pointer shadow-lg transition-all duration-300`}>{children}</button>
     )
 }
 
